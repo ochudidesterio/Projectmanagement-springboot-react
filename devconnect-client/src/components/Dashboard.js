5a7d9ffc6b97ff2629b74ac74f68f6649789e1f2
DESTERIO-RASMENTO <desochudi@gmail.com>
2021-04-03 23:12:43 +0300

    create client

---
+++ devconnect-client/src/components/Dashboard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import CreateProjectButton from './Project/CreateProjectButton'
+import ProjectItem from './Project/ProjectItem'
+
+const  Dashboard=() =>{
+  return (
+    <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <CreateProjectButton/>
+                    <br />
+                    <hr />
+                  <ProjectItem/>
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Dashboard
